d7379562e0b21eb91ec6b9e94b1ea967e71c8013
DSzym <127440793+DSzym@users.noreply.github.com>
2024-11-14 16:22:01 +0100

    Przygotowanie promptów do testowania

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ prompt = ""
 f = open("Zadanie dla Junior AI Developera - tresc artykulu.txt", "r")
 text = f.read()
 
+prompt1 = "Podany dokument przekształć na kod HTML, który można umieścić między tagami <body> i </body>. Użyj odpowiednich tagów HTML dla nagłówków i zwykłego tekstu."
+prompt2 = "Podany dokument przekształć na kod HTML, który można umieścić między tagami <body> i </body>. Użyj odpowiednich tagów HTML dla nagłówków i zwykłego tekstu. Nie dodawaj nowych nagłówków. Wyznacz miejsca, w których warto wstawić grafiki i oznacz je tagiem <img> z atrybutem src=image_placeholder.jpg."
+prompt3 = "Podany dokument przekształć na kod HTML, który można umieścić między tagami <body> i </body>. Użyj odpowiednich tagów HTML do strukturyzacji treści. Wyznacz miejsca, w których warto wstawić grafiki i oznacz je tagiem <img> z atrybutem src=image_placeholder.jpg. Do każdej grafiki dodaj atrybut alt z promptem do wygenerowania grafiki. Pod grafikami umieść podpisy używając odpowiednich tagów."
 
 """def text_to_html():
     response = openai.ChatCompletion.create(
